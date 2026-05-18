6062cf5e1e4a2ec5b5aac6ed023158252ce0eba5
Sandesh Singh <sandesh04singh@gmail.com>
2026-05-18 05:06:30 +0000

    comparisons in JS

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,37 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+
+// console.log("2" > 1); // here string converted to number for comaprison
+// console.log("02" > 1); // here also
+
+
+//avoid these type of conversion
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0); //here null converted to 1 thats why giving true
+
+// console.log(undefined == 0)
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+//Strict check(===)
+
+console.log("2" === 2)
+console.log(2 === 2);
+
+
+
+
+
+
+
+
+
+
+
