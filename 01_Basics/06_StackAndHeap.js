35258cbab1f477102a842f78e2d7288a88accea9
Sandesh Singh <sandesh04singh@gmail.com>
2026-05-18 07:29:31 +0000

    Stack and Heap

---
+++ 01_Basics/06_StackAndHeap.js
@@ -0,0 +1,23 @@
+// Stack(primitive), Heap(Non-primitive)
+
+let myYTName = "fgdsugfdsugfudsk"
+
+let anotherName = myYTName
+anotherName = "chaiaurcode"
+
+console.log(myYTName)
+console.log(anotherName);
+
+
+
+let userOne = {
+    email:"sat@gmail.com",
+    upi:"user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "user@google.com"
+console.log(userOne.email)
+console.log(userTwo.email);
+
