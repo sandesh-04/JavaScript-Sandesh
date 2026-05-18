1077c0635f31e70a897cd15198a918ef578b782d
Sandesh Singh <sandesh04singh@gmail.com>
2026-05-18 03:33:14 +0000

    Variables in JS

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 2331312
+let accountEmail = "snjfksjf@gmail.com"
+var accountPass = "73t823t"
+/*
+prefer not to use var because
+of issue in blockScope or functional Scope
+
+*/
+
+accountCity = "Benagaluru"
+let accountState;
+
+accountId = 2 //Not allowed as Constant can not be changed
+
+accountEmail = "shdgjwdf@gmail.com"
+accountPass = "t468712g"
+accountCity = "Varanasi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+//maintain readability of code and it should be high
+// console.log(3 +
+//     3
+// );  // node a good practice,
+
+
+let name = "JavaScript"
+let age = 32
+let isLoggedInt = false
+
+
+// primitive data types
+
+//number => 2 to power 52
+//bigint
+//String => " " or ' '
+//boolean => true/false
+//null  => standalone value
+//undefined => when we have not assigned the value
+//symbol => to specify the unique objects
+
+
+//Objects
+
+console.log(typeof "JavaScirpt");
+
+console.log(typeof null); // Object
+
+console.log(typeof undefined); // undefined
+
