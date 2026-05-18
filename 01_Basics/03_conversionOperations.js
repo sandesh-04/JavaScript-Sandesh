0efab582a586a7fa2946715e9c55655dfaa0ce96
Sandesh Singh <sandesh04singh@gmail.com>
2026-05-18 04:29:41 +0000

    conversion and operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,103 @@
+let score = 33
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let value1 = "33abc" // null // true 
+let valueInNumber1 = Number(value1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1);
+
+
+// 33 => 33
+// "33abc" = NaN(Not a number)
+//true => 1 , false => 0
+
+let isLoggedIn = 1 // "" //"sandesh" //0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false, "sandesh" => true
+
+let num = 33
+let stringNumber = String(num)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ******************Operations****************
+
+let val = 3
+let negVal = -val
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);// 2 to the power 2
+// console.log(2**3);// 2 to the power 3
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "sandesh"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((2 + 3) * 5) // use parenthesis for good code writing
+
+
+// console.log(true)  // true
+// console.log(+true)  // 1
+
+// console.log(true+)  //error
+
+// console.log(+"") //0
+
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.log(num3);
+
+
+
+//prefix postfix
+let n1 = 100
+// n1++ //++n1
+// console.log(n1);
+
+// console.log(++n1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
